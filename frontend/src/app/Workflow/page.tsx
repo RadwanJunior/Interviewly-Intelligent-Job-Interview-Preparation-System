252cdb0d75e4b731e95239a47cc28757f92e001c
Marwan <mmash102@uottawa.ca>
2025-03-20 17:37:23 -0400

    add route protection and fixes

--- frontend/src/app/Workflow/page.tsx
+++ frontend/src/app/Workflow/page.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import { WorkflowProvider } from "@/context/WorkflowContext";
 import WorkflowLayout from "@/components/workflow/WorkflowLayout";
 import WorkflowStageRenderer from "@/components/workflow/WorkflowStageRenderer";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 const ResumeUpload = () => {
   return (
-    <WorkflowProvider>
-      <WorkflowLayout title="Resume Upload">
-        <WorkflowStageRenderer />
-      </WorkflowLayout>
-    </WorkflowProvider>
+    <ProtectedRoute>
+      <WorkflowProvider>
+        <WorkflowLayout title="Resume Upload">
+          <WorkflowStageRenderer />
+        </WorkflowLayout>
+      </WorkflowProvider>
+    </ProtectedRoute>
   );
 };
 
